133c6405e87496e46b53d5c79c70274af2af9676
Sarves14 <71976758+Sarves14@users.noreply.github.com>
2020-12-12 11:42:29 +0530

    Delete store.js

--- react_app/src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import { createStore } from 'redux';
-import reducer from './demoReducer.js';
-import logger from './middleware/logger';
-
-
-const store = configureStore({ reducer, middleware: [...getDefaultMiddleware(), logger] });
-export default store;
